ef860b2a58fb6dbcb18159f7b890998db1b454bd
Jennings Liu <jennings.liu@sap.com>
2017-03-20 17:49:48 +0800

    split the script into two parts, conf-generator and executor

--- Kubernetes/Deployment/Containerized/scripts/templates/shell/gen_kubeconfig.sh
+++ Kubernetes/Deployment/Containerized/scripts/templates/shell/gen_kubeconfig.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 apiserver=$1
 outdir=$2
-
-if ! which kubectl;then 
+kubeconfig=$3
+if ! which kubectl;then
     echo "install kubectl"
      curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl -o /usr/bin/kubectl
 fi
 
-kubectl config set-cluster devosp-k8s --certificate-authority=/data/kubernetes/certs/ca.pem  --server=https://${apiserver}:6443 --kubeconfig=${outdir}/kubeconfig
-kubectl config set-credentials admin  --client-certificate=/data/kubernetes/certs/apiserver.pem --client-key=/data/kubernetes/certs/apiserver-key.pem  --kubeconfig=${outdir}/kubeconfig
+kubectl config set-cluster devosp-k8s --certificate-authority=/data/kubernetes/certs/ca.pem  --server=https://${apiserver}:6443 --kubeconfig=${outdir}/${kubeconfig}
+kubectl config set-credentials admin  --client-certificate=/data/kubernetes/certs/apiserver.pem --client-key=/data/kubernetes/certs/apiserver-key.pem  --kubeconfig=${outdir}/${kubeconfig}
 kubectl config set-context admin@devosp-k8s --cluster=devosp-k8s --user=admin  --kubeconfig=${outdir}/kubeconfig
-kubectl config  use-context admin@devosp-k8s  --kubeconfig=${outdir}/kubeconfig
+kubectl config  use-context admin@devosp-k8s  --kubeconfig=${outdir}/${kubeconfig}

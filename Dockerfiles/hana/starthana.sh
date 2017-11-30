f04c33e2d79afe79ca38d41579d2602b71b2df2e
Jennings Liu <jennings.liu@sap.com>
2017-11-30 16:24:37 +0800

    add hana docker file

---
+++ Dockerfiles/hana/starthana.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ -f /hana/data/SID ]] then 
+    su - ${SID}adm
+    HDB start
+else
+    /hana/shared/SAP_HANA_DATABASE/hdblcm --batch --action=install --components=all --sid=${SID} --number=${INSTANCE_NB}   -password=${PASSWORD} -sapadm_password=${PASSWORD}  -system_user_password=${PASSWORD}  --sapmnt=/hana/shared --datapath=/hana/data --logpath=/hana/log 
+    echo ${SID} >/hana/data/SID
+fi

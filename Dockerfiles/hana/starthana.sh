6c8ef81271f79e9163405cdec7723419b6ea78c6
Jennings Liu <jennings.liu@sap.com>
2017-12-01 20:19:01 +0800

    fix su user issue

--- Dockerfiles/hana/starthana.sh
+++ Dockerfiles/hana/starthana.sh
@@ -10,18 +10,18 @@ if [ -f /hana/data/SID ]; then
     groupadd -g ${GID} ${group}
     useradd  -d /usr/sap/${SID}/home -u ${uid} -s /bin/sh  -g sapsys -G ${group}  ${user}
     echo "export SID=${SID}" >>/etc/profile
-    su - ${user} 
-    source /usr/sap/${SID}/home/.profile
-    HDB start
+    su - ${user} -c " \
+    source /usr/sap/${SID}/home/.profile  && \
+    HDB start "
 
 else
     /hana/shared/SAP_HANA_DATABASE/hdblcm --batch --action=install --components=all --sid=${SID} --number=${INSTANCE_NB}   -password=${PASSWORD} -sapadm_password=${PASSWORD}  -system_user_password=${PASSWORD}  --sapmnt=/hana/shared --datapath=/hana/data --logpath=/hana/log 
     echo ${SID} >/hana/data/SID
     echo "export SID=${SID}" >>/etc/profile
     user=$(echo "${SID}adm"|awk '{print tolower($0)}')
-    su - ${user}
-    id|awk '{print $1}'|awk -F\( '{print $1}'|awk -F\= '{print $2}' >/usr/sap/${SID}/${SID}.uid
-    id|awk '{print $2}'|awk -F\( '{print $1}'|awk -F\= '{print $2}' >/usr/sap/${SID}/${SID}.gid
-    id|awk '{print $3}'|awk -F, '{print $2}'|awk -F\( '{print $1}' >/usr/sap/${SID}/${SID}.Gid
-    chown ${SID} /usr/sap/${SID}/${SID}.uid  /usr/sap/${SID}/${SID}.gid  /usr/sap/${SID}/${SID}.Gid
+    su - ${user} -c " \
+    id|awk '{print $1}'|awk -F\( '{print $1}'|awk -F\= '{print $2}' >/usr/sap/${SID}/${SID}.uid && \
+    id|awk '{print $2}'|awk -F\( '{print $1}'|awk -F\= '{print $2}' >/usr/sap/${SID}/${SID}.gid && \
+    id|awk '{print $3}'|awk -F, '{print $2}'|awk -F\( '{print $1}' >/usr/sap/${SID}/${SID}.Gid && \
+    chown ${SID} /usr/sap/${SID}/${SID}.uid  /usr/sap/${SID}/${SID}.gid  /usr/sap/${SID}/${SID}.Gid"
  fi

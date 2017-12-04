33c55bb81e65b6e2ec79290b11ab2c63b00252bb
Jennings Liu <jennings.liu@sap.com>
2017-12-04 13:57:31 +0800

    modify stophana.sh

--- Dockerfiles/hana/stophana.sh
+++ Dockerfiles/hana/stophana.sh
@@ -3,11 +3,11 @@
 SID=$(cat /hana/data/SID)
 user=$(echo "${SID}adm"|awk '{print tolower($0)}')
 group=$(echo "${SID}shm"|awk '{print tolower($0)}')
-uid= $(cat /usr/sap/${SID}/${SID}.uid)
+uid=$(cat /usr/sap/${SID}/${SID}.uid)
 gid=$(cat /usr/sap/${SID}/${SID}.gid)
 GID=$(cat /usr/sap/${SID}/${SID}.Gid)
 groupadd -g ${gid} sapsys
 groupadd -g ${GID} ${group}
-useradd  -d /usr/sap/${SID}/home -u ${uid} -s /bin/sh  -gsapsys -G ${group}  ${user}
+useradd  -d /usr/sap/${SID}/home -u ${uid} -s /bin/sh  -g sapsys -G ${group}  ${user}
 su - ${user} -c " source /usr/sap/${SID}/.profile && HDB stop"
 
